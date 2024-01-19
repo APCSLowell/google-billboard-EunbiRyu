6970797f77dea0d7b94117c8bcc1234431e711a7
mrchanr <42151767+mrchanr@users.noreply.github.com>
2024-01-19 14:07:38 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -4,7 +4,7 @@ public class GoogleBillboard {
 	public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435"; 
 
 	//Think of main(String[] args) as Java's setup
-	public void main(String[] args){
+	public static void main(String[] args){
 		String digits = e.substring(2,12);
 	  	double myNum = Double.parseDouble(digits);
 	 	for(int i=2; i < e.length()-10; i++){
